917897debcc5615b172f9352196c021c10d2abf7
JFK <jfkquality@gmail.com>
2018-02-19 22:08:31 -0500

    Refactor: Make solution a function answer(numbers).

--- pirate-challenge.js
+++ pirate-challenge.js
@@ -1,44 +1,50 @@
-// Randomly generate numbers[]; no index can have itself as a value
-// Loop through numbers.
-// Keep track of redirected numbers/indices. In an array?
+// Randomly generate pirates[]; no index can have itself as a value
+// Loop through pirates.
+// Keep track of redirected numbers/indices. In an array.
 // If current redirected number already visited, then there's a loop.
 // Count number of redirections in that loop and return that value.
 
-// Create random numbers list, the length of the # of pirates.
-let  pirates = 0;
-while (pirates < 2 || pirates > 5000) {
-    pirates = prompt("How many pirates are there (2-5000)?");
+let  piratesCount = 0;
+while (piratesCount < 2 || piratesCount > 5000) {
+    piratesCount = prompt("How many pirates are there (2-5000)?");
 }
-let redirect = [];
-let loop = [];
-let redirected = "";
+let pirates = [];
 let out = "";
 
-for (x = 0; x < pirates; x++) {
-    noSelfRedirect(redirect, pirates);
+// Create random numbers list, the length of the # of pirates.
+for (x = 0; x < piratesCount; x++) {
+    noSelfRedirect(pirates, piratesCount);
 }
-out += "<p>" + pirates + " pirates passing the buck: " + redirect + "<p>";
+out += "<p>" + piratesCount + " pirates passing the buck: " + pirates + "<p>";
 
-let currentRedirect = 0;
-while (loop.indexOf(currentRedirect) == -1) {
-    loop.push(currentRedirect);
-    currentRedirect = redirect[currentRedirect];
+let piratesLoop = answer(pirates);
+out += "<p>Oh no! Caught in a loop! " + piratesLoop + "<p>";
+
+for (let i = 0; i < piratesLoop.length; i++) {
+    console.log(piratesLoop[i]);
+    out += "<p>" + piratesLoop[i] + "</p>";
 }
-out += "<p>On the trail... " + loop + "<p>";
-out += "<p>Oh no! Caught in the loop! " + loop.slice(loop.indexOf(currentRedirect)) + "<p>";
 out += "<p>Somebody help me!!</p>";
+
 this.document.getElementById("result").innerHTML = out;
 
-// for (let i = loop.indexOf(currentRedirect); i < loop.length; i++) {
-//     console.log(loop[i]);
-// }
-
 function noSelfRedirect(arr, max) {
-    redirected = Math.floor(Math.random() * (max));
+    let redirected = Math.floor(Math.random() * (max));
     if (redirected != arr.length && redirected != arr.indexOf(redirected)) {
         arr.push(redirected);
     } else {
       noSelfRedirect(arr, max);
     }
-  
+}
+
+function answer(numbers) {
+    let loop = [];
+    let currentRedirect = 0;
+    while (loop.indexOf(currentRedirect) == -1) {
+        loop.push(currentRedirect);
+        currentRedirect = numbers[currentRedirect];
+    }
+    out += "<p>On the trail... " + loop + "<p>";
+    loop = loop.slice(loop.indexOf(currentRedirect));
+    return loop;    
 }
